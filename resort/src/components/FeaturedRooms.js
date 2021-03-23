666cd9e7b43ff593e67984fa64cf16c886cc2805
fakhrirf <fakhririyadh@gmail.com>
2021-03-23 12:12:28 +0700

    message

--- resort/src/components/FeaturedRooms.js
+++ resort/src/components/FeaturedRooms.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {RoomContext} from '../context';
 import Loading from "./Loading";
 import Room from "./Room";
 import Title from './Title';
 
-export default class FeaturedRooms extends Component {
-static contextType = RoomContext;
-    render() {
-        let {loading, featuredRooms : rooms} = this.context;
-        rooms = rooms.map (room => {
-            return <Room key={room.id} room={room} />;
-        });
+const FeaturedRooms = () => {
+    const context = useContext(RoomContext);
+ 
+        // access value from context
+       let {featuredRooms: rooms, loading } = context;
+
+        rooms = rooms.map(room =>{
+            return <Room key={room.id}
+                            room={room}/>
+        })
+
         return (
-            <section className="featured-rooms">
-                <Title title="Featured Rooms" />
+            <section className='featured-rooms'>
+                <Title title='featured-rooms'/>
+
                 <div className="featured-rooms-center">
-                    {loading? <Loading/> : rooms}
+                    {loading ? <Loading/> : rooms}
+ 
                 </div>
             </section>
         )
-    }
-}
- 
+    };
+
+    export default FeaturedRooms;

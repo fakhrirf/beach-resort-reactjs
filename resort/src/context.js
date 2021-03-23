d4a92b2f9240e93b707c8da53e5f4e83c44c0ee3
fakhrirf <fakhririyadh@gmail.com>
2021-03-23 17:07:49 +0700

    messages

--- resort/src/context.js
+++ resort/src/context.js
@@ -12,6 +12,14 @@ class RoomProvider extends Component {
         sortedRooms:[],
         featuredRooms:[],
         loading: true,
+        type: "all",
+        capacity:1,
+        price:0,
+        minPrice:0,
+        maxPrice: 0,
+        minSize:0,
+        breakfast: false,
+        pets: false,
     };
 
 // getData
@@ -20,19 +28,26 @@ class RoomProvider extends Component {
         //This .getData
         let rooms = this.formatData(items)
         let featuredRooms = rooms.filter(room => room.featured === true);
+        
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size))
+        
         this.setState({
             rooms, 
             featuredRooms, 
             sortedRooms:rooms, 
-            loading:false
+            loading:false,
+            price:maxPrice,
+            maxPrice,
+            maxSize,
         });
     }
+
 
     formatData(items){ 
         let tempItems = items.map(item => {
             let id = item.sys.id;
             let images = item.fields.images.map(image => image.fields.file.url);
-
             let room = {...item.fields, images, id};
             return room;
         })
@@ -46,6 +61,20 @@ class RoomProvider extends Component {
         return room;
     };
 
+    handleChange = event => {
+        const target = event.target
+        const value = event.type === 'cheeckbox' ? target.checked:target.value
+        const name = event.target.name;
+        this.setState({
+            [name]:value
+        }, this.filterRooms) 
+        // console.log(`this is type: ${type}, this is name: ${name}, this is value: ${value}`);
+    };
+
+
+    filterRooms = () => {
+        console.log('hello');
+    }
 
     render() {
         return (
@@ -64,5 +93,14 @@ class RoomProvider extends Component {
 
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component){
+    return function ConsumerWrapper(props){
+        return <RoomConsumer>
+            {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+    }
+}
+
 
 export{RoomProvider, RoomConsumer, RoomContext};
--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Hero from "../components/Hero";
 import {Link} from "react-router-dom";
-
+import RoomContainer from '../components/RoomContainer';
 
 const Rooms = () => {
-    return (
+    return ( 
         <>
-        <Hero hero="roomsHero">
-            <Banner title="Our rooms ">
-                <Link to='/' className="btn-primary">
-                return home
-                </Link>
-            </Banner>
-        </Hero>
+            <Hero hero="roomsHero">
+                <Banner title="our rooms">
+                    <Link to='/' className="btn-primary">
+                    return home
+                    </Link>
+                </Banner>
+            </Hero>
+            <RoomContainer />
         </>
     );
 }
